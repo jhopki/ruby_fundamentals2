0940c2463a6ba984b9abccb643f9c16489f6df82
Julia Hopkins <jfhopkins6@gmail.com>
2013-10-23 12:39:18 -0400

    Exercise 6, half finished

---
+++ exercise6.rb
@@ -0,0 +1,16 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+def write_list(list)
+  list.each {|x| puts "* #{x}"}
+end
+write_list(grocery_list)
+grocery_list << "rice"
+write_list(grocery_list)
+
+puts "#{grocery_list.length} items"
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
