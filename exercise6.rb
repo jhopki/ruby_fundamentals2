a4fbddad3724c50fa7583603ebf0b6d392ea2fc5
Julia Hopkins <jfhopkins6@gmail.com>
2013-10-23 12:44:22 -0400

    Exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -14,3 +14,10 @@ if grocery_list.include?("bananas")
 else
   puts "You don't need to pick up bananas today"
 end
+
+puts "Second item: #{grocery_list[1]}"
+
+write_list(grocery_list.sort!)
+
+grocery_list.delete("salmon")
+write_list(grocery_list)
